44a71bc7dd82465019c0666a8100e75b8cb2376e
Aron Fuentes <aronsss1231@gmail.com>
2025-11-25 00:02:05 -0400

    registropropiedades

--- alquila360_backend/src/app.module.ts
+++ alquila360_backend/src/app.module.ts
@@ -1,3 +1,4 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
@@ -7,35 +8,43 @@ import { UserModule } from './user/user.module';
 import { HController } from './h/h.controller';
 import { AuthModule } from './auth/auth.module';
 import { PropertyModule } from './property/property.module';
+import { User } from './entity/user.entity'; 
+import { Property } from './property/property.entity'; // ✅ Importación de Property
+import { Image } from './property/image.entity'; 
 
 @Module({
-  imports: [
-    // 1. CONFIGURACIÓN DEL ENTORNO
-    ConfigModule.forRoot({
-      isGlobal: true, 
-      envFilePath: '.env', // Aún necesario para el S3Service (aunque lo usamos local)
-    }),
-    
-    // 2. CONFIGURACIÓN PRINCIPAL DE TYPEORM (SOLUCIÓN MANUAL DE EMERGENCIA)
-    TypeOrmModule.forRoot({
-      type: 'mysql', 
-      host: 'localhost',      // ⬅️ Manualmente 'localhost'
-      port: 3306,             // ⬅️ Manualmente 3306
-      username: 'root',       // ⬅️ Manualmente 'root'
-      password: '12345678',   // ⬅️ Manualmente '12345678'
-      database: 'alquila360',  // ⬅️ Manualmente 'alquila360'
-      
-      autoLoadEntities: true, 
-      synchronize: true, 
-    }),
+  imports: [
+    // 1. CONFIGURACIÓN DEL ENTORNO
+    ConfigModule.forRoot({
+      isGlobal: true, 
+      envFilePath: '.env',
+    }),
+    
+    // 2. CONFIGURACIÓN PRINCIPAL DE TYPEORM (SOLUCIÓN MANUAL DE EMERGENCIA)
+   TypeOrmModule.forRoot({
+      type: 'mysql', 
+      host: 'localhost',      
+      port: 3306,             
+      username: 'root',       
+      password: '12345678',   
+      database: 'alquila360',
+      
+     
+      entities: [
+        User,       
+        Property, // ⬅️ ¡AÑADIDA! Era el eslabón perdido que 'Image' necesita.
+        Image,
+      ],
+      
+      synchronize: true, 
+    }),
 
-    // 3. MÓDULOS DE LA APLICACIÓN
-    // Asegúrate de que UserModule, AuthModule, y PropertyModule vayan DESPUÉS.
-    UserModule, 
-    AuthModule,
-    PropertyModule, 
-  ],
-  controllers: [AppController, HController],
-  providers: [AppService],
+    // 3. Módulos de la Aplicación
+    UserModule, 
+    AuthModule,
+    PropertyModule, 
+  ],
+  controllers: [AppController, HController],
+  providers: [AppService],
 })
 export class AppModule {}
